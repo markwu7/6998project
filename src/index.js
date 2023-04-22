f847b3dfdf4fb2fc7068cdba9d26c7ad60e7715b
grossmann4 <grossmann4@yahoo.com>
2023-04-22 12:29:02 -0400

    work in progress

--- src/index.js
+++ src/index.js
@@ -3,10 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-import { Amplify } from 'aws-amplify';
+import { Amplify, API} from 'aws-amplify';
 import awsExports from './aws-exports';
 Amplify.configure(awsExports);
-
+Amplify.configure({
+  aws_cloud_logic_custom: [
+    {
+      name: 'projectapp', // (required) - API Name (This name is used used in the client app to identify the API - API.get('your-api-name', '/path'))
+      endpoint: 'https://dw9ehzs68l.execute-api.us-east-1.amazonaws.com/stage1', // (required) -API Gateway URL + environment
+      region: 'us-east-1' // (required) - API Gateway region
+    }
+  ]
+});
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
